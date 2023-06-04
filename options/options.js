e19072cc59ee1923ca4b7f4d95221df0132923c6
Crowdbotics <engineering@crowdbotics.com>
2023-06-04 23:12:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-paper-dev-88550",
+  url: "https://withered-paper-dev-88550.botics.co",
+  api: "https://withered-paper-dev-88550.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
